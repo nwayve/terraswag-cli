7374a6b23d557eeaa1f25d2d8d0cb1647e97563e
Dennis Evans <dennis.evans@cleardata.com>
2017-07-16 15:17:32 -0500

    Updated method.spec to handle rest_api and resource types for resource_id

--- src/swagger-converter/aws-api-gateway-method/method.configuration.ts
+++ src/swagger-converter/aws-api-gateway-method/method.configuration.ts
@@ -3,6 +3,7 @@ import { HttpVerbs, AuthorizationTypes } from './index';
 export interface ApiMethodConfiguration {
     name: string;
     serviceName: string;
+    resourceName?: string;
     httpVerb: HttpVerbs;
     authorizationType: AuthorizationTypes;
 }
--- src/swagger-converter/aws-api-gateway-method/method.ts
+++ src/swagger-converter/aws-api-gateway-method/method.ts
@@ -1,26 +1,46 @@
-// Vendor
+// vendor
 import { compile } from 'handlebars';
 import { mapValues } from 'lodash';
 
-// Project
+// project
 import { ApiMethodConfiguration, HttpVerbs, AuthorizationTypes } from './index';
 
-const TEMPLATE =
-`resource "aws_api_gateway_method" "{{method.name}}" {
+// *Note: The 'resource_id' property is intentionally missing a trailing '}'
+//          handlebars can't deal with trailing '}}}', so induded it in 'identifier' value.
+const TEMPLATE = `\
+resource "aws_api_gateway_method" "{{method.name}}" {
   rest_api_id   = "\${aws_api_gateway_rest_api.{{method.serviceName}}.id}"
-  resource_id   = "\${aws_api_gateway_rest_api.{{method.serviceName}}.root_resource_id}"
+  resource_id   = "\${aws_api_gateway_{{resource.type}}.{{resource.name}}.{{resource.identifier}}"
   http_method   = "{{method.httpVerb}}"
   authorization = "{{method.authorizationType}}"
 }
 `;
 export class AwsApiGatewayMethod {
     template = compile(TEMPLATE);
+    resourceConfig: object;
 
-    constructor(private config: ApiMethodConfiguration) { }
+    constructor(private config: ApiMethodConfiguration) {
+        this.resourceConfig = {
+            type: 'rest_api',
+            name: config.serviceName,
+            identifier: 'root_resource_id}'
+        };
+    }
 
     toTerraformString(): string {
+        const methodConfig = mapValues(this.config, this.configValueMapper);
+
+        if (methodConfig.resourceName && methodConfig.resourceName.length) {
+            this.resourceConfig = {
+                type: 'resource',
+                name: methodConfig.resourceName,
+                identifier: 'id}'
+            };
+        }
+
         return this.template({
-            method: mapValues(this.config, this.configValueMapper)
+            method: methodConfig,
+            resource: this.resourceConfig
         });
     }
 
