26c497bcabfd0725ba29e37a4ce9c28440fbb290
Dennis Evans <dennis.evans@cleardata.com>
2017-08-06 22:13:23 -0500

    Created test lambda integration swagger doc and added it to test/data

--- test/data/index.ts
+++ test/data/index.ts
@@ -3,3 +3,4 @@ import { SwaggerDocument } from '../../lib/swagger-converter';
 export const basicSwaggerDoc: SwaggerDocument = require('./minimal-swagger-doc.json');
 export const serviceSwaggerDoc: SwaggerDocument = require('./service-swagger-doc.json');
 export const advancedSwaggerDoc: SwaggerDocument = require('./test-service-development-swagger-integrations.json');
+export const lambdaIntegrationSwaggerDoc: SwaggerDocument = require('./lambda-integration.swagger.json');
